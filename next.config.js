ab2bf3bfefc6f2bbd014f61e6936f05fdfa43bf0
Kevin Carrington <Kevington99@gmail.com>
2024-08-11 13:30:32 -0700

    Fix basePath configuration

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: 'snaptogether',
+  basePath: '/snaptogether',
   images: {
     unoptimized: true,
   },
